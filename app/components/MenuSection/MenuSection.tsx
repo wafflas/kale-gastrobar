ba4ebd9ec35bb7867f566e05618c50dc55b95ccc
wafflas <giannikakisgiwrgos@gmail.com>
2026-01-31 22:58:51 +0200

    update MenuSection

--- app/components/MenuSection/MenuSection.tsx
+++ app/components/MenuSection/MenuSection.tsx
@@ -1,8 +1,6 @@
 function MenuSection() {
   return (
-    <section className="relative min-h-screen lg:min-h-screen w-full bg-darkbrown flex items-center overflow-visible rounded-bl-[500px] rounded-tr-[200px]">
-      MenuSection
-    </section>
+    <section className="relative min-h-screen lg:min-h-screen w-full bg-darkbrown flex items-center overflow-visible rounded-bl-[500px] rounded-tr-[200px]"></section>
   );
 }
 
