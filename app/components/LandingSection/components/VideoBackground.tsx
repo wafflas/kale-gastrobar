dc5d509be25fc35451d28a509072615de1dce226
wafflas <giannikakisgiwrgos@gmail.com>
2026-03-04 00:57:45 +0200

    reverted the positioning on the landing page

--- app/components/LandingSection/components/VideoBackground.tsx
+++ app/components/LandingSection/components/VideoBackground.tsx
@@ -94,7 +94,7 @@ export default function VideoBackground({
           playsInline
           preload="auto"
           key={videoSrc}
-          className="absolute inset-0 w-full h-full object-cover"
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full w-auto h-auto object-cover"
         >
           <source src={videoSrc} type="video/mp4" />
           Your browser does not support the video tag.
